99c476baf5527134c3feeae0a1680a23888be1ae
Ji Kim <jiyoung.kimmy@gmail.com>
2023-12-01 11:55:07 -0500

    reorganzing/formatting:

--- shortestpossiblepath.py
+++ shortestpossiblepath.py
@@ -1,10 +1,10 @@
-# -------- Function File --------------------
+# -------- Function File ----------------------------------------------------------
 '''
   1. Below you will find the function that returns the shortest possible path from a start word to an end word.
   2. Please review the Specifications.md file for more details on the conditions of the function and its parameters.
   3. Thank you and happy searching for the shortest path!
 '''
-
+#  ---------- MAIN FUNCTION -------------------------------------------------------
 def shortestPossiblePath(start, end, dictionaryList):
   dictionarySet = set(dictionaryList)
   visitedHashmap = dict()
@@ -31,29 +31,46 @@ def shortestPossiblePath(start, end, dictionaryList):
     print("current queue is: ", queue)
     count += 1
 
-
+#  ---------- SUB FUNCTIONS -------------------------------------------------------
+#  ------- ADD A LETTER ----------------
 def addLetter(wordInfo, hashmap):
   # return all possible transition words that exist in the dict after adding a letter to the current word
   transitionWords = []
-  print("letter added")
+  alphabets = 'abcdefghijklmnopqrstuvwxyz'
+  word = wordInfo[0]
+  pathLength = wordInfo[1]
+
+  for i in range(len(word)):
+    for char in alphabets:
+      transition = word[:i] + char + word[i:]
+
+
   return transitionWords
 
+#  ------- DELETE A LETTER --------------------------------------------------------
 def deleteLetter(wordInfo, hashmap):
   # return all possible transition words that exist in the dict after adding a letter to the current word
   transitionWords = [["yellow", 2], ["bye", 2]]
   print("letter deleted")
   return transitionWords
 
+#  ------- CHANGE A LETTER --------------------------------------------------------
 def changeLetter(wordInfo, hashmap):
   # return all possible transition words that exist in the dict after adding a letter to the current word
   transitionWords = [["fellow", 2]]
+  alphabets = 'abcdefghijklmnopqrstuvwxyz'
+
   print("switched from the ABC's")
   return transitionWords
 
+#  ------- CREATE LIST FROM START TO END WITH THE SHORTEST PATHWAY ----------------
 def resultList(hashmap):
   # use end word to traverse back through the
   print("visted dictionary ", hashmap)
 
+
+
+# --------- TEST EXAMPLES ---------------------------------------------------------
 first = "tag"
 last = "bowl"
 listOfWords = ["tag", "tar", "bar", "boar", "boa", "bow", "bowl"]
