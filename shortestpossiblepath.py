c38f5aa7d95c18899da7ab7310272809e60d6387
Ji Kim <jiyoung.kimmy@gmail.com>
2023-12-01 14:07:54 -0500

    No pathway example added

--- shortestpossiblepath.py
+++ shortestpossiblepath.py
@@ -133,9 +133,11 @@ first = "tag"
 last = "bowl"
 listOfWords1 = ["tag", "tar", "bar", "boar", "boa", "bow", "bowl", "fowl", "rag", "tal", "bal", "bol"]
 listOfWords2 = ["tag", "tar", "bar", "boar", "boa", "bow", "bowl", "fowl", "rag"]
+listOfWords3 = ["tag", "tar", "bar", "boar", "bow", "bowl", "fowl", "rag"]
 
 print(shortestPossiblePath(first, last, listOfWords1))
 print(shortestPossiblePath(first, last, listOfWords2))
+print(shortestPossiblePath(first, last, listOfWords3)) # return None
 
 
 start = "hit"
