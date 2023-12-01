0807c8eff4f4d95d5991454e4a9dcab98ba12629
Ji Kim <jiyoung.kimmy@gmail.com>
2023-12-01 10:22:06 -0500

    place holders for sub functions

--- shortestpossiblepath.py
+++ shortestpossiblepath.py
@@ -16,18 +16,35 @@ class ShortestPossiblePath:
     # To make look up in the dictionary O(1) time complexity as opposed to O(N), where N is equal to the number of words in dictionaryList
     self.dictionarySet = set(dictionaryList)
 
+  def addLetter(self, wordInfo):
+    # return all possible transition words that exist in the dict after adding a letter to the current word
+    print("letter added")
+
+  def deleteLetter(self, wordInfo):
+    # return all possible transition words that exist in the dict after adding a letter to the current word
+    print("letter deleted")
+
+  def changeLetter(self, wordInfo):
+    # return all possible transition words that exist in the dict after adding a letter to the current word
+    print("switched from the ABC's")
+
   def shortestPathway(self):
     # Implementing a Breadth First Search:
     # queue = deque([self.start, 1])
     # queue = Queue([self.start, 1])
+    # regular array pop(0) is a shift -- can I make this a better time complexity later?
     queue = [[self.start, 1]]
-    count = 1
+
     while queue:
       current = queue.pop(0)
       currentWord = current[0]
       currentLength = current[1]
-      print("popped: ", current, currentWord, currentLength, count)
-      count += 1
+
+      self.addLetter(currentWord)
+
+
+
+
     # print("dictionary is: ", self.dictionarySet, self.dictionaryList)
 
   # def addLetter(self):
